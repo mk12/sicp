33fb1351e25cab7e13fc0a75750b21eb207c3a2e
Mitchell Kember <mk12360@gmail.com>
2020-11-27 22:12:02 -0500

    Trying rust-script for linter

---
+++ lint.rs
@@ -0,0 +1,12 @@
+#!/usr/bin/env rust-script
+
+//! This script lints Scheme files to ensure proper formatting. See the "Style"
+//! section in README.md for details on the style.
+//!
+//! ```cargo
+//! [dependencies]
+//! ```
+
+fn main() {
+    println!("hi");
+}
